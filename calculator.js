c889fc1dea32f682d12d18e2a675863afdddb9d8
Connor Ketcheson <connorketcheson01@gmail.com>
2022-08-23 16:18:24 +0900

    create separate functions for input

--- calculator.js
+++ calculator.js
@@ -20,24 +20,33 @@ function divide(a, b) {
     result = a / b;
 };
 
+
+
 //buttons for operations
 const addition = document.getElementById('plus').addEventListener('click', () => {
     operation = 1;
-    document.getElementById('result').innerHTML = "";
+    input2(operation);
+    document.getElementById('result').innerHTML = "+";
 });
 const subtraction = document.getElementById('minus').addEventListener('click', () => {
     operation = 2;
-    document.getElementById('result').innerHTML = "";
+    input2(operation);
+    document.getElementById('result').innerHTML = "-";
 });
 const multiplication = document.getElementById('multiply').addEventListener('click', () => {
     operation = 3;
-    document.getElementById('result').innerHTML = "";
+    input2(operation);
+    document.getElementById('result').innerHTML = "×";
 });
 const division = document.getElementById('divide').addEventListener('click', () => {
     operation = 4;
-    document.getElementById('result').innerHTML = "";
+    input2(operation);
+    document.getElementById('result').innerHTML = "÷";
 });
 const operate = document.getElementById('equals').addEventListener('click', () => {
+    console.log(num1);
+    console.log(num2);
+    console.log(operation);
     if (operation === 1) {
         result = add(num1, num2);
     } else if (operation === 2) {
@@ -49,31 +58,36 @@ const operate = document.getElementById('equals').addEventListener('click', () =
     } else {
         alert("Please input a number!")
     };
+    console.log(result);
     document.getElementById('result').innerHTML = result;
     operation = 0;
 });
 
 //store the first and second integers using the selected numbers
 //switch from variable num1 to num2 when an operation has been selected
-function input(o) {
-    if (o === 0) {
-        for (let i = 0; i < 10; i++) {
-            document.getElementById(`button${i}`).addEventListener('click', () => {
-                primary.push(i)
-                num1 = +primary.join("");
-                document.getElementById('result').innerHTML = num1;
-            })
-        };
-    } else if (o !== 0) {
-        for (let i = 0; i < 10; i++) {
-            document.getElementById(`button${i}`).addEventListener('click', () => {
-                secondary.push(i)
-                num2 = +secondary.join("");
-                document.getElementById('result').innerHTML = num2;
-            })
-        };
+function input1(o) {
+    //if (o === 0) {
+    for (let i = 0; i < 10; i++) {
+        document.getElementById(`button${i}`).addEventListener('click', () => {
+            primary.push(i)
+            num1 = +primary.join("");
+            document.getElementById('result').innerHTML = num1;
+        })
     };
 };
+//};
+
+function input2(o) {
+    //if (o !== 0) {
+    for (let i = 0; i < 10; i++) {
+        document.getElementById(`button${i}`).addEventListener('click', () => {
+            secondary.push(i)
+            num2 = +secondary.join("");
+            document.getElementById('result').innerHTML = num2;
+        })
+    };
+};
+//};
 
 const clear = document.getElementById('clear').addEventListener('click', () => {
     document.getElementById('result').innerHTML = "";
@@ -85,5 +99,6 @@ const clear = document.getElementById('clear').addEventListener('click', () => {
     operation = 0;
 });
 
-input (operation);
+
+
 
