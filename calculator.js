d71d10aab524515b31c1a6dd70419e73e2a55f29
Connor Ketcheson <connorketcheson01@gmail.com>
2022-08-24 10:14:30 +0900

    complete operation for first iteration

--- calculator.js
+++ calculator.js
@@ -1,84 +1,96 @@
 //store values selected by the user
 let primary = [];
+let mem = null;
 let num1 = null;
 let secondary = [];
 let num2 = null;
 let operation = 0;
 let result = null;
+let iteration = 0;
 
 //functions for the four operations used by this calculator
-function add(a, b) {
-    result = a + b;
-};
-function subtract(a, b) {
-    result = a - b;
-};
-function multiply(a, b) {
-    result = a * b;
-};
-function divide(a, b) {
-    result = a / b;
+function calculate(a, b) {
+    if (operation === 1) {
+        return a + b
+    } else if (operation === 2) {
+        return a - b
+    } else if (operation === 3) {
+        return a * b 
+    } else if (operation === 4) {
+        return a / b
+    };
 };
 
+// function init(operation) {
+//     if (operation !== 0) {
+//         return
+//     } else {
+//         input1();
+//     };
+// };
 
+// init(0);
+
+input1();
 
 //buttons for operations
 const addition = document.getElementById('plus').addEventListener('click', () => {
     operation = 1;
-    input2(operation);
+    num1 = mem;
+    input2();
+    // init(1);
     document.getElementById('result').innerHTML = "+";
 });
 const subtraction = document.getElementById('minus').addEventListener('click', () => {
     operation = 2;
-    input2(operation);
+    num1 = mem
+    input2();
+    // init(2);
     document.getElementById('result').innerHTML = "-";
 });
 const multiplication = document.getElementById('multiply').addEventListener('click', () => {
     operation = 3;
-    input2(operation);
+    num1 = mem
+    input2();
+    // init(3);
     document.getElementById('result').innerHTML = "×";
 });
 const division = document.getElementById('divide').addEventListener('click', () => {
     operation = 4;
-    input2(operation);
+    num1 = mem
+    input2();
+    // init(4);
     document.getElementById('result').innerHTML = "÷";
 });
 const operate = document.getElementById('equals').addEventListener('click', () => {
-    console.log(num1);
-    console.log(num2);
-    console.log(operation);
-    if (operation === 1) {
-        result = add(num1, num2);
-    } else if (operation === 2) {
-        result = subtract(num1, num2);
-    } else if (operation === 3) {
-        result = multiply(num1, num2);
-    } else if (operation === 4) {
-        result = divide(num1, num2);
-    } else {
-        alert("Please input a number!")
-    };
-    console.log(result);
-    document.getElementById('result').innerHTML = result;
+    //if (iteration > 0) {
+    //    num1 = result;
+    //} 
+    calculate(num1, num2);
+    console.log(calculate(num1, num2));
+    document.getElementById('result').innerHTML = calculate(num1, num2);
     operation = 0;
+    iteration ++;
 });
 
 //store the first and second integers using the selected numbers
 //switch from variable num1 to num2 when an operation has been selected
-function input1(o) {
-    //if (o === 0) {
+function input1() {
+    // if (iteration > 0) {
+    //     return 
+    // } else {
     for (let i = 0; i < 10; i++) {
         document.getElementById(`button${i}`).addEventListener('click', () => {
             primary.push(i)
-            num1 = +primary.join("");
-            document.getElementById('result').innerHTML = num1;
+            mem = +primary.join("");
+            document.getElementById('result').innerHTML = mem;
         })
     };
 };
-//};
+// };
 
-function input2(o) {
-    //if (o !== 0) {
+function input2() {
+    // if (operation !== 0) {
     for (let i = 0; i < 10; i++) {
         document.getElementById(`button${i}`).addEventListener('click', () => {
             secondary.push(i)
@@ -87,17 +99,31 @@ function input2(o) {
         })
     };
 };
-//};
+// };
 
 const clear = document.getElementById('clear').addEventListener('click', () => {
     document.getElementById('result').innerHTML = "";
     result = null;
     primary = [];
+    mem = null;
     num1 = null; 
     secondary = [];
     num2 = null;
     operation = 0;
+    // iteration = 0;
 });
+
+// function init(operation) {
+//     if (operation != 0) {
+//         return
+//     } else {
+//         input1();
+//     };
+// };
+
+// init(0);
+
+//input1(operation);
 
 
 
